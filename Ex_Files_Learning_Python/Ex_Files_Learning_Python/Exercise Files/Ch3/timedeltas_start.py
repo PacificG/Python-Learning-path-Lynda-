a8b2254fea284602b0cef6a6da356943dee67b34
1pk73 <Prashantkkumar1998@gmail.com>
2019-08-29 22:54:56 +0530

    learning python

--- Ex_Files_Learning_Python/Ex_Files_Learning_Python/Exercise Files/Ch3/timedeltas_start.py
+++ Ex_Files_Learning_Python/Ex_Files_Learning_Python/Exercise Files/Ch3/timedeltas_start.py
@@ -26,12 +26,14 @@ print(str(now + timedelta(days=2,weeks=3)))
 print(str(now - timedelta(weeks=1)))
 
 ### How many days until April Fools' Day?
-
-
+april = datetime(2019,5,5)
+print(str(now - april))
 # use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
-
+if now.month > april.month:
+    april.replace(2020)
+print(april)
 
 # Now calculate the amount of time until April Fool's Day  
-
-
+days = str(april - now) if now.year < april.year else str(now - april)
+print(days)
--- Ex_Files_Learning_Python/Ex_Files_Learning_Python/Exercise Files/Ch5/inetdata_start.py
+++ Ex_Files_Learning_Python/Ex_Files_Learning_Python/Exercise Files/Ch5/inetdata_start.py
@@ -1,7 +1,13 @@
 # 
 # Example file for retrieving data from the internet
 #
+import urllib.request
 
+def main():
+  url = urllib.request.urlopen('http://www.google.com')
+  print('result code:' + str(url.getcode()))
 
+  data = url.read()
+  print(data)
 if __name__ == "__main__":
   main()
